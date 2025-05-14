1cfc36524374aacd143b1a61b3f977e46b6c04d4
loopinfinity20 <learner.nexus24@gmail.com>
2025-05-14 14:24:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// let boxes = document.querySelector(".box");
+// let resetBtn = document.querySelector("#resate-btn");
+
+// let turn0 = true;
+
+// const winingPatterns = [
+//     [0,1,2]
+//     [0,3,6]
+//     [0,4,8]
+//     [1,4,7]
+//     [2,5,8]
+//     [2,4,6]
+//     [3,4,5]
+//     [6,7,8]
+// ];
+
+// boxes.forEach((box) => {
+//     box.addEventListener("click", () => {
+//         console.log("box was clicked");
+//     })
+// });
+let heading = document.querySelector('h1');
+heading.innerHTML = `<u>${heading.innerText}</u>`;
